8168ab04b060dcb597b6fd7959cd13a3c4e838a4
DESIGNSBY.AM <andrew@sorzery.com>
2021-12-13 21:18:07 -0600

    Made some progress

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'white': '#ffffff',
+        'maintext': '#595959',
+        'navbg': '#06061b',
+        'navborder': '#353443',
+        'navbghover': '#161626',
+        'navborderhover': '#6e67ad',
+        'navbgcurrent': '#262630',
+        'gem': '#6e67ad'
+      }
+    },
+  },
+  plugins: [],
+}
